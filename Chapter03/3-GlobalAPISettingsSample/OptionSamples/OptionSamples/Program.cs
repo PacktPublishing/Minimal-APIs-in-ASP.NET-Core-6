8871a20988ecb1c13fb8ec0f2c29619752763be0
Andrea Tosato <andrea.tosato@hotmail.it>
2021-11-28 17:44:00 +0100

    Update with Snapshot

--- Chapter03/3-GlobalAPISettingsSample/OptionSamples/OptionSamples/Program.cs
+++ Chapter03/3-GlobalAPISettingsSample/OptionSamples/OptionSamples/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.Configure<OptionBasic>(builder.Configuration.GetSection("OptionBasic"));
 builder.Services.Configure<OptionMonitor>(builder.Configuration.GetSection("OptionMonitor"));
+builder.Services.Configure<OptionSnapshot>(builder.Configuration.GetSection("OptionSnapshot"));
 builder.Services.Configure<OptionCustomName>("CustomName1", builder.Configuration.GetSection("CustomName1"));
 builder.Services.Configure<OptionCustomName>("CustomName2", builder.Configuration.GetSection("CustomName2"));
 
@@ -19,16 +20,18 @@ if (app.Environment.IsDevelopment())
 
 app.MapGet("/read/options", (IOptions<OptionBasic> optionsBasic,
     IOptionsMonitor<OptionMonitor> optionsMonitor,
+    IOptionsSnapshot<OptionSnapshot> optionsSnapshot,
     IOptionsFactory<OptionCustomName> optionsFactory) =>
 {
 
-    return new
+    return Results.Ok(new
     {
         Basic = optionsBasic.Value,
         Monitor = optionsMonitor.CurrentValue,
+        Snapshot = optionsSnapshot.Value,
         Custom1 = optionsFactory.Create("CustomName1"),
         Custom2 = optionsFactory.Create("CustomName2")
-    };
+    });
 })
 .WithName("ReadOptions");
 
@@ -38,16 +41,21 @@ namespace OptionSamples
 {
     public class OptionBasic
     {
-        public string? Value { get; set; }
+        public string? Value { get; init; }
+    }
+
+    public class OptionSnapshot
+    {
+        public string? Value { get; init; }
     }
 
     public class OptionMonitor
     {
-        public string? Value { get; set; }
+        public string? Value { get; init; }
     }
 
     public class OptionCustomName
     {
-        public string? Value { get; set; }
+        public string? Value { get; init; }
     }
 }
