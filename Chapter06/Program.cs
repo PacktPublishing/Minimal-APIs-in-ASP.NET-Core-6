71fa371868dd0add4867c6f802f8ff6cf5421770
Marco Minerva <marco.minerva@gmail.com>
2021-12-21 17:31:15 +0100

    Add empty project

---
+++ Chapter06/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
