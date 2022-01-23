11920cb7db5d8b5f7573d91a4e3c737aea8d4016
Marco Minerva <marco.minerva@gmail.com>
2022-01-23 17:31:22 +0100

    FluentValidation sample update

--- Chapter06/Program.cs
+++ Chapter06/Program.cs
@@ -139,7 +139,10 @@ public class PersonValidator : AbstractValidator<Person>
 {
     public PersonValidator()
     {
-        RuleFor(p => p.FirstName).NotEmpty().MaximumLength(30);
+        RuleFor(p => p.FirstName).NotEmpty()
+            .WithMessage("You must provide the first name")
+            .MaximumLength(30);
+
         RuleFor(p => p.LastName).NotEmpty().MaximumLength(30);
         RuleFor(p => p.Email).EmailAddress().Length(6, 100);
     }
